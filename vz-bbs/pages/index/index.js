61ad31931051251911f13957faedc0bb0e60c56a
Stephen <704199902@qq.com>
2016-12-07 15:45:48 +0800

    save

--- vz-bbs/pages/index/index.js
+++ vz-bbs/pages/index/index.js
@@ -3,7 +3,9 @@
 var app = getApp()
 Page({
   data: {
-    "hidTop": false
+    "hidTop": false,
+    "showSearch":false,
+    "showComment":false,
   },
   //事件处理函数
   bindViewTap: function () {
@@ -27,10 +29,40 @@ Page({
    */
   scroll: function (e) {
     let {scrollLeft, scrollTop, scrollHeight, scrollWidth, deltaX, deltaY} = e.detail;
-    if (scrollTop && scrollTop > 500) {
+    if (scrollTop && scrollTop > 200) {
       this.setData({ "hidTop": true })
     } else {
       this.setData({ "hidTop": false })
     }
+  },
+  /**
+   * 展示搜索框
+   */
+  search: function(e){
+    console.log("搜索", e)
+    this.setData({"showSearch":false})
+  },
+  /**
+   * 关闭搜索框
+   */
+  cancleSearch: function(e) {
+    console.log("取消搜索")
+    this.setData({"showSearch":false})
+  },
+  /**
+   * 展示评论框
+   */
+  showComment: function(e) {
+    console.log("展示评论框", e)
+    this.setData({"showComment":true})
+  },
+  /**
+   * 关闭评论框
+   */
+  cancleComment: function(e) {
+    console.log("关闭评论框", e)
+    this.setData({"showComment":false})
   }
+
+
 })
